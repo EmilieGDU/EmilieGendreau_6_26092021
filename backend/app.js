e4a0be9fa4f8442bbb9c6f492f2d6dd8ed7b4a37
EmilieGDU <emilie.gendreau@yahoo.fr>
2021-12-19 13:10:23 +0100

    Express : création du serveur

--- backend/app.js
+++ backend/app.js
@@ -2,23 +2,61 @@ const express = require("express");
 
 const app = express();
 
+// express.json() inbuilt method to recognize the incoming request object as a JSON object, and to allow access to the body of the request contained in req.body 
+app.use(express.json());
+
+// CORS error prevention (use method to apply to all routes)
 app.use((req, res, next) => {
-    console.log("Requête reçue");
+    res.setHeader("Access-Control-Allow-Origin", "*");
+    res.setHeader("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content, Accept, Content-Type, Authorization");
+    res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, PATCH, OPTIONS");
     next();
 });
 
-app.use((req, res, next) => {
-    res.status(201);
-    next();
+// Routes
+app.use("/endpoint", (req, res, next) => { // Endpoint = route pour laquelle nous souhaitons enregistrer cet élément de middleware = URL demandée par l'application frontend
+    // Création des objets à renvoyer selon le schéma de données spécifique requis par le frontend
+    const objetARenvoyer = {
+
+    };
+    // Ajout du code de statut à l'objet response et renvoi des données sous forme de JSON
+    res.status(XXX).json(objetARenvoyer);
 });
 
-app.use((req, res, next) => {
-    res.json({message : "Voici la réponse finale du serveur !"});
-    next();
+app.post("/api/auth/signup", (req, res, next) => {
+    res.status(201).json({
+        message: ""
+    });
 });
 
-app.use((req, res) => {
-    console.log("Réponse envoyée avec succès");
+app.post("/api/auth/login", (req, res, next) => {
+    res.status(201).json({
+        userId: "",
+        token: ""
+    });
+});
+
+app.post("/api/sauces", (req, res, next) => {
+    res.status(201).json({
+        message: ""
+    });
+});
+
+app.post("/api/sauces/:id/like", (req, res, next) => {
+    res.status(201).json({
+        message: ""
+    });
+});
+
+
+app.get("/api/sauces", (req, res, next) => {
+    const sauces = [];
+    res.status(200).json(sauces);
+});
+
+app.get("/api/sauces/:id", (req, res, next) => {
+    const sauce = {};
+    res.status(200).json(sauce);
 });
 
 module.exports = app ;
