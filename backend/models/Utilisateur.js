d073378a2e34e031b08547c397e752e643e2020b
EmilieGDU <emilie.gendreau@yahoo.fr>
2021-12-28 23:10:17 +0100

    Mongoose : application du plugin mongoose-unique-validator au schéma de données utilisateur

--- backend/models/Utilisateur.js
+++ backend/models/Utilisateur.js
@@ -1,8 +1,14 @@
 const mongoose = require("mongoose");
 
+// 2 users cannot have the same email address
+const uniqueValidator = require("mongoose-unique-validator");
+
 const utilisateurSchema = mongoose.Schema({
     email: {type: String, required: true, unique: true},
-    password: {type: String}
+    password: {type: String, required: true}
 });
+
+// Application of the plugin to the Schema before making a model
+utilisateurSchema.plugin(uniqueValidator);
 
 module.exports = mongoose.model("Utilisateur", utilisateurSchema);
