9ed11fa7aaef55819fa4b08cfb404e4ed5866111
EmilieGDU <emilie.gendreau@yahoo.fr>
2021-12-29 01:02:04 +0100

    Mongoose : implémentation de la fonction signup avec utilisation du package de chiffrement bcrypt

--- backend/controllers/utilisateurs.js
+++ backend/controllers/utilisateurs.js
@@ -1,10 +1,21 @@
+const bcrypt = require("bcrypt");
+
 // Importing Mongoose model
 const Utilisateur = require("../models/Utilisateur");
 
 exports.signup = (req, res, next) => {
-    res.status(201).json({
-        message: ""
-    });
+    // Password encryption with salting over 10 turns
+    bcrypt.hash(req.body.password, 10)
+    .then((hash) => {
+        const utilisateur = new Utilisateur({
+            email: req.body.email,
+            password: hash
+        });
+        utilisateur.save()
+            .then(() => res.status(201).json({message: "Utilisateur créé." }))
+            .catch((error) => res.status(400).json({error}));
+    })
+    .catch((error) => res.status(500).json({error}));
 };
 
 exports.login = (req, res, next) => {
