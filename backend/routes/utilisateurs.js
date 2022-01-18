e688a1281cde63b69ffe1382ebb1fd3d80385cb1
EmilieGDU <emilie.gendreau@yahoo.fr>
2022-01-18 15:51:22 +0100

    Express : ajout du middleware express-rate-limit

--- backend/routes/utilisateurs.js
+++ backend/routes/utilisateurs.js
@@ -3,13 +3,18 @@ const express = require("express");
 // Creating an Express router
 const router = express.Router();
 
+// Importing middlewares
+const rateLimit = require("../middleware/rate_limit");
+
+
 // Importing utilisateurs controller
 const utilisateurCtrl = require("../controllers/utilisateurs");
 
 
 // Implementing individuals routes in the router
+// Applying middlewares and assigning controller functions
 router.post("/signup", utilisateurCtrl.signup);
-router.post("/login", utilisateurCtrl.login);
+router.post("/login", rateLimit, utilisateurCtrl.login);
 
 
 module.exports = router;
